d2206ba620d39f51b341dc89c7e7dba7e03ba82a
FedericoDang95 <federicodang93@gmail.com>
2024-07-12 12:21:53 +0200

    add:primo test

---
+++ app.js
@@ -0,0 +1,12 @@
+/**Chiedere all’utente di inserire una parola Creare una funzione per capire se la parola inserita è palindroma */
+
+let parola = prompt('inserisci una parola')
+console.log(parola);
+
+function parolaPalindroma(parola) {
+
+    // converto la parola a lettere minuscole
+    parola = parola.toLowerCase();
+    // creo una versione invertita della parola
+    let p
+}
